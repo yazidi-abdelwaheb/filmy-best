d5df89496a78c4447e5f1f4622eeb87dc10da3e7
abdelwaheb11 <yazidiabdelwaheb@gmail.com>
2025-12-17 00:38:51 +0100

    fixed categories liste

--- src/app/back_office/modules/client/categories/categories.component.ts
+++ src/app/back_office/modules/client/categories/categories.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import Category from '../../../shared/models/category.model';
+import { CategoriesService } from '../../../shared/services/categories.service';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  standalone: true, // si tu utilises Angular standalone
+  imports: [NgFor],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
 
+  categories: Category[] = [];
+
+  page: number = 1;
+  limit: number = 5;
+
+  constructor(private cs: CategoriesService) {}
+
+  loadData() {
+    this.cs.list(this.page, this.limit).subscribe(data => {
+      // si data est un tableau
+      this.categories = [...this.categories, ...data];
+    });
+  }
+
+  showMore() {
+    this.page++;
+    this.loadData();
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
 }
